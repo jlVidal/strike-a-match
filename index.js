8dc3a8a5380336e9447807c37d748f4b1f094afb
Nick Fisher <spadgos@gmail.com>
2015-10-26 18:56:11 +0100

    Refactor lib to be a bit more JS'y, add some more tests

--- index.js
+++ index.js
@@ -1,108 +1,52 @@
-/**
- * Created by John on 9/23/2015.
- */
+module.exports = {
+    compareStrings: compareStrings
+};
+
+function compareStrings(a, b) {
+    a = a.trim().toUpperCase();
+    b = b.trim().toUpperCase();
+
+    // shortcut out of some situations...
+    // identical strings
+    if (a === b) return 1;
+    // one empty string
+    if (!a || !b) return 0;
+    // both one char
+    if (a.length === 1 && b.length === 1) return 0;
+
+    // one string is one char
+    if (a.length === 1) return b.indexOf(a) > -1 ? 1 / b.length : 0;
+    if (b.length === 1) return a.indexOf(b) > -1 ? 1 / a.length : 0;
+
+    var pairs1 = wordLetterPairs(a);
+    var pairs2 = wordLetterPairs(b);
+
+    var union = pairs1.length + pairs2.length;
+
+    var intersection = pairs1.filter(function (pair1) {
+        var match = pairs2.indexOf(pair1);
+        if (match > -1) {
+            pairs2.splice(match, 1); // Must remove the match to prevent "GGGG" from appearing to match "GG" with 100% success
+            return true;
+        }
+    }).length;
+
+    return (2 * intersection) / union;
+}
+
 function letterPairs(str) {
     var numPairs = str.length - 1;
-
     var pairs = new Array(numPairs);
     for (var i = 0; i < numPairs; i++) {
         pairs[i] = str.substring(i, i + 2);
     }
-
     return pairs;
 }
-function isBlank(str) {
-    return (!str || /^\s*$/.test(str));
-}
 function wordLetterPairs(str) {
-    var allPairs = [];
-
-    // Tokenize the string and put the tokens/words into an array
-    var words = str.split(/(\s)/);
-
-    // For each word
-    for (var w = 0; w < words.length; w++) {
-        if (!isBlank(words[w])) {
-            // Find the pairs of characters
-            var pairsInWord = letterPairs(words[w]);
-            for (var p = 0; p < pairsInWord.length; p++) {
-                allPairs.push(pairsInWord[p]);
-            }
-        }
-    }
-
-    return allPairs;
+    return flatten(str.split(/\s+/).map(function (word) {
+        return letterPairs(word);
+    }));
 }
-var toExport =
-{
-    compareStrings: function (a, b) {
-        if (!a || !b) {
-            if (!a && !b)
-                return 1;
-
-            return 0;
-        }
-
-        a = a.toUpperCase();
-        b = b.toUpperCase();
-        if (a.length == 1 && a.length == 1) {
-            if (a === b) {
-                return 1;
-            }
-            return 0;
-        }
-
-        var pairs1 = wordLetterPairs(a);
-        var pairs2 = wordLetterPairs(b);
-
-        if (pairs1.length == 0 || pairs2.length == 0) {
-            a = a.trim();
-            b = b.trim();
-
-            var maxSize = Math.max(a.length, b.length);
-            var percentUnit = 1 / maxSize;
-            var score = 0;
-            for (var i = 0; i < maxSize; i++) {
-                var tempLetterA;
-
-                if (i + 1 >= a.length)
-                    tempLetterA = null;
-                else
-                    tempLetterA = a.substring(i, i + 1);
-
-                var tempLetterB;
-
-                if (i + 1 >= b.length) {
-                    tempLetterB = null;
-                }
-                else {
-                    tempLetterB = b.substring(i, i + 1);
-                }
-
-                if (tempLetterA == null || tempLetterB == null)
-                    return score;
-
-                if (tempLetterA === tempLetterB)
-                    score = score + percentUnit;
-            }
-            return score;
-        }
-
-        var intersection = 0;
-        var union = pairs1.length + pairs2.length;
-
-        for (var i = 0; i < pairs1.length; i++) {
-            for (var j = 0; j < pairs2.length; j++) {
-                if (pairs1[i] === pairs2[j]) {
-                    intersection++;
-                    pairs2.splice(j, 1);//Must remove the match to prevent "GGGG" from appearing to match "GG" with 100% success
-
-                    break;
-                }
-            }
-        }
-
-        return (2.0 * intersection) / union;
-    }
-};
-module.exports = toExport;
+function flatten(arrays) {
+    return Array.prototype.concat.apply([], arrays);
+}
--- index.test.js
+++ index.test.js
@@ -1,46 +1,55 @@
-/**
- * Created by John on 9/24/2015.
- */
 var comparison = require('./index');
+var compareStrings = comparison.compareStrings;
 
 var expect = require('chai').expect;
 
 describe('strike-a-match', function () {
-    it('should work', function () {
-        expect(true).to.be.true;
-    });
-
     describe('compareStrings', function () {
         it('should be 100% equal', function () {
-            expect(comparison.compareStrings("Strike a match", "Strike a match")).to.equal(1);
+            expect(compareStrings("Strike a match", "Strike a match")).to.equal(1);
+        });
+        it('should treat empty and non-empty strings as entirely different', function () {
+            expect(compareStrings('', 'a')).to.equal(0);
+        });
+        it('should treat two whitespace-only strings as equal', function () {
+            expect(compareStrings('', ' ')).to.equal(1);
+        });
+
+        it('should trim strings', function () {
+            expect(compareStrings('a ', ' a')).to.equal(1);
+        });
+        it('should perform case insensitive comparisons', function () {
+            expect(compareStrings('A', 'a')).to.equal(1);
+        });
+
+        it('should work on short strings', function () {
+            expect(compareStrings('a', 'ab')).to.equal(0.5);
+            expect(compareStrings('a', 'abcd')).to.equal(0.25);
+        });
+
+        it('should not match repeated strings with 100% success', function () {
+            expect(compareStrings('gggg', 'gg')).to.equal(0.5);
         });
 
         it('should return different percentages of equality', function () {
-            // (Healed)
-            // Sealed	80%
-            // Healthy	55%
-            // Heard	44%
-            // Herded	40%
-            // Help	    25%
-            // Sold	    0%
             var keyWord = "Healed";
-            expect(comparison.compareStrings(keyWord, "Sealed")).to.equal(0.8);
-            expect(Math.round(comparison.compareStrings(keyWord, "Healthy") * 100) / 100).to.equal(0.55);
-            expect(Math.round(comparison.compareStrings(keyWord, "Heard") * 100) / 100).to.equal(0.44);
-            expect(comparison.compareStrings(keyWord, "Help")).to.equal(0.25);
-            expect(comparison.compareStrings(keyWord, "Sold")).to.equal(0.0);
+            expect(compareStrings(keyWord,       "Sealed")).to.equal(0.8);
+            expect(round(compareStrings(keyWord, "Healthy"))).to.equal(0.55);
+            expect(round(compareStrings(keyWord, "Heard"))).to.equal(0.44);
+            expect(compareStrings(keyWord,       "Help")).to.equal(0.25);
+            expect(compareStrings(keyWord,       "Sold")).to.equal(0);
         });
 
         it('should respect a proper proximity', function () {
             var keyWord = "FRANCE";
 
-            var resultOne = comparison.compareStrings(keyWord, "REPUBLIC OF FRANCE");
-            expect(Math.round(resultOne * 100) / 100).to.equal(0.56);
-            var resultTwo = comparison.compareStrings(keyWord, "QUEBEC");
+            var resultOne = compareStrings(keyWord, "REPUBLIC OF FRANCE");
+            expect(round(resultOne)).to.equal(0.56);
+            var resultTwo = compareStrings(keyWord, "QUEBEC");
             expect(resultTwo).to.equal(0);
 
-            var resultThree = comparison.compareStrings(keyWord, "FRENCH REPUBLIC");
-            var resultFour = comparison.compareStrings(keyWord, "REPUBLIC OF CUBA");
+            var resultThree = compareStrings(keyWord, "FRENCH REPUBLIC");
+            var resultFour = compareStrings(keyWord, "REPUBLIC OF CUBA");
 
             expect(resultOne).to.be.above(resultTwo).above(resultThree).above(resultFour);
         });
@@ -50,11 +59,15 @@ describe('strike-a-match', function () {
             var compareWithFirst = "REPUBLIC OF FRANCE";
             var compareWithSecond = "REPUBLIC OF CUBA";
 
-            var resultOne = comparison.compareStrings(keyWord, compareWithFirst);
-            expect(Math.round(resultOne * 100) / 100).to.equal(0.72);
-            var resultTwo = comparison.compareStrings(keyWord, compareWithSecond);
-            expect(Math.round(resultTwo * 100) / 100).to.equal(0.61);
+            var resultOne = compareStrings(keyWord, compareWithFirst);
+            expect(round(resultOne)).to.equal(0.72);
+            var resultTwo = compareStrings(keyWord, compareWithSecond);
+            expect(round(resultTwo)).to.equal(0.61);
             expect(resultOne).to.be.above(resultTwo);
         });
     })
 });
+
+function round(x) {
+    return Math.round(x * 100) / 100;
+}
